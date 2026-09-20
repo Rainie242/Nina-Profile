f20d5ba939ca8c82d719314d6767f1f9c2cc5774
Rainie242 <a0972026993@gmail.com>
2026-09-20 11:16:33 +0800

    update shared files

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,12 @@
 import { years } from "./model/years.js";
 import { AuthorName } from "./model/AuthorName.js";
 
-years();
-AuthorName();
+$('#headerArea').load('../components/header/header.html', function () {
+    const nowpage = $('body').data('page');
+    $(`button[data-page="${nowpage}"]`).addClass('active');
+})
+
+$('#footerArea').load('../components/footer/footer.html', function () {
+    years();
+    AuthorName();
+})
