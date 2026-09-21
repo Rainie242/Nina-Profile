73548971a3a6d21fc83eb78722129cb61c2618d5
Rainie242 <a0972026993@gmail.com>
2026-09-21 10:57:53 +0800

    header&footer 修改

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,12 +1,12 @@
 import { years } from "./model/years.js";
 import { AuthorName } from "./model/AuthorName.js";
 
-$('#headerArea').load('../components/header/header.html', function () {
+$('#headerArea').load('./components/header/header.html', function () {
     const nowpage = $('body').data('page');
     $(`button[data-page="${nowpage}"]`).addClass('active');
 })
 
-$('#footerArea').load('../components/footer/footer.html', function () {
+$('#footerArea').load('./components/footer/footer.html', function () {
     years();
     AuthorName();
 })
